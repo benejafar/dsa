1fbba6014697c4e1c76addd2139bc9665d965da6
mjafarka <muhammed.abdulnazer@ibsplc.com>
2023-12-14 20:04:51 +0530

    added print statement in Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        SuccessorWithDelete swd = new SuccessorWithDelete();
-        swd.createList(10);
-        swd.remove(5);
-        swd.remove(6);
-        System.out.println(swd.successor(5));
+        System.out.println("this is from Company laptop");
         // System.out.println(swd.successor(6));
 
         // SWD swd = new SWD();
