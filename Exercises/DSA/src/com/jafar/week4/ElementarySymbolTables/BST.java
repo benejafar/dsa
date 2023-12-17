76efd272994a56645ce20a88a95fc18f310bc3f7
mjafarka <muhammed.abdulnazer@ibsplc.com>
2023-12-17 09:42:43 +0530

    after creating BST and  Binary Tree

--- Exercises/DSA/src/com/jafar/week4/ElementarySymbolTables/BST.java
+++ Exercises/DSA/src/com/jafar/week4/ElementarySymbolTables/BST.java
@@ -1,5 +1,7 @@
 package com.jafar.week4.ElementarySymbolTables;
 
+import edu.princeton.cs.algs4.Queue;
+
 public class BST<Key extends Comparable<Key>, Value> {
 
     private Node root;
@@ -8,10 +10,12 @@ public class BST<Key extends Comparable<Key>, Value> {
         private Key key;
         private Value val;
         private Node left, right;
+        private int count;
 
-        public Node(Key key, Value val) {
+        public Node(Key key, Value val, int count) {
             this.key = key;
             this.val = val;
+            this.count = count;
         }
     }
 
@@ -21,7 +25,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 
     public Node put(Node x, Key key, Value val) {
         if (x == null)
-            return new Node(key, val);
+            return new Node(key, val, 1);
         int cmp = key.compareTo(x.key);
         if (cmp < 0) {
             x.left = put(x.left, key, val);
@@ -29,7 +33,18 @@ public class BST<Key extends Comparable<Key>, Value> {
             x.right = put(x.right, key, val);
         } else
             x.val = val;
+        // for including count
+        x.count = 1 + size(x.left) + size(x.right);
         return x;
+    }
+
+    public int size(){
+        return size(root);
+    }
+
+    public int size(Node x){
+        if (x == null) return 0;
+        return x.count;
     }
 
     public Value get(Key key) {
@@ -47,9 +62,57 @@ public class BST<Key extends Comparable<Key>, Value> {
         return null;
     }
 
-    public Key floor(Key key)
-    {
-        
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null)
+            return null;
+        else
+            return x.key;
     }
+
+    public Node floor(Node x, Key key) {
+
+        if (x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0)
+            return x;
+        else if (cmp < 0)
+            return floor(x.left, key);
+
+        Node t = floor(x.right, key);
+        if (t != null)
+            return t;
+        else
+            return x;
+    }
+
+    public int rank(Key key){
+        return rank(key,root);
+    }
+
+    private int rank(Key key, Node x){
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)    return rank(key, x.left);
+        else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
+        else  return size(x.left);
+    }
+
+    public Iterable<Key> keys(){
+        Queue<Key> q = new Queue<>();
+        inorder(root,q);
+        return q;
+    }
+
+    private void inorder(Node x, Queue<Key> q){
+        if (x == null) return;
+
+        inorder(x.left,q);
+        q.enqueue(x.key);
+        inorder(x.right, q);
+    }
+
+    
 
 }
